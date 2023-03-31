8d1c94d00b30315ca39954ab802621688a97557e
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-03-31 12:37:00 +0500

    Association:Applied has_one type of association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_063640) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_072519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,6 +111,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_063640) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sub_articles", force: :cascade do |t|
+    t.string "title"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_sub_articles_on_article_id"
+  end
+
   add_foreign_key "author_projects", "authors"
   add_foreign_key "author_projects", "projects"
+  add_foreign_key "sub_articles", "articles"
 end
